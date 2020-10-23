d6f34fa73d32e0fa2a7f1bc1bbb822c4da31bfd5
d2rivendell <dream2rivendell@gmail.com>
2020-10-23 18:45:16 +0800

    update

--- SwiftDataStructure/SwiftDataStructure/testCase/BRTreeTest.swift
+++ SwiftDataStructure/SwiftDataStructure/testCase/BRTreeTest.swift
@@ -12,9 +12,19 @@ func testBRtree(){
     example("BRtree test") {
         let tree =  RBTree<Int>()
         let nums = [55, 87, 56, 74, 96, 22, 62, 20, 70, 68, 90, 50]
+        print("添加用例：")
         for n in nums {
            tree.add(n)
         }
         print(tree.description)
+        print("删除用例：")
+        for (i, n) in nums.enumerated() {
+           print("====================")
+           print("after deleting \(n)")
+           tree.remove(e: n)
+           print(tree.description)
+           assert(tree.size == nums.count - i - 1)
+           print("====================")
+        }
     }
 }
